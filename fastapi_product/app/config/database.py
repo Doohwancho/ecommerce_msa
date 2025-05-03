4167cdf93f8e802b0805bffeeeade73711d3ebda
Doohwancho <doohwancho1993@gmail.com>
2025-05-03 23:49:14 +0900

    build: mongodb가 standalone이었는데 replica set(write & read)로 변경. monstache가 실시간 동기화 하려면 반드시 replica set이어야 함

--- fastapi_product/app/config/database.py
+++ fastapi_product/app/config/database.py
@@ -16,11 +16,23 @@ engine = create_async_engine(SQLALCHEMY_DATABASE_URL)
 async_session = sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
 Base = declarative_base()
 
+# mongodb 연결 설정
+MONGODB_HOST = os.getenv("MONGODB_HOST", "mongodb-service")
+MONGODB_REPLICA_SET = os.getenv("MONGODB_REPLICA_SET", "rs0")
+MONGODB_AUTH_SOURCE = os.getenv("MONGODB_AUTH_SOURCE", "admin")
+
+
+
 # MongoDB 비동기 설정
 async def get_product_collection() -> AgnosticCollection:
     try:
+        # MongoDB 연결 설정
         mongo_client = AsyncIOMotorClient(
-            f"mongodb://{os.getenv('MONGODB_USERNAME')}:{os.getenv('MONGODB_PASSWORD')}@{os.getenv('MONGODB_URL')}"
+            # before replica set
+            # f"mongodb://{os.getenv('MONGODB_USERNAME')}:{os.getenv('MONGODB_PASSWORD')}@{os.getenv('MONGODB_URL')}"
+            # after replica set
+            # f"mongodb://{os.getenv('MONGODB_USERNAME')}:{os.getenv('MONGODB_PASSWORD')}@mongodb-stateful-0.mongodb-service.default.svc.cluster.local:27017,mongodb-stateful-1.mongodb-service.default.svc.cluster.local:27017/?replicaSet=rs0&authSource=admin"
+            f"mongodb://{os.getenv('MONGODB_USERNAME')}:{os.getenv('MONGODB_PASSWORD')}@{MONGODB_HOST}:27017/?replicaSet={MONGODB_REPLICA_SET}&authSource={MONGODB_AUTH_SOURCE}"
         )
         # 명시적으로 데이터베이스와 컬렉션 생성
         mongo_db = mongo_client.my_db
--- fastapi_user/app/config/database.py
+++ fastapi_user/app/config/database.py
@@ -2,10 +2,18 @@ import os
 from motor.motor_asyncio import AsyncIOMotorClient
 from motor.core import AgnosticCollection
 
+# mongodb 연결 설정
+MONGODB_HOST = os.getenv("MONGODB_HOST", "mongodb-service")
+MONGODB_REPLICA_SET = os.getenv("MONGODB_REPLICA_SET", "rs0")
+MONGODB_AUTH_SOURCE = os.getenv("MONGODB_AUTH_SOURCE", "admin")
+
 async def get_users_collection() -> AgnosticCollection:
     try:
         mongo_client = AsyncIOMotorClient(
-            f"mongodb://{os.getenv('MONGODB_USERNAME')}:{os.getenv('MONGODB_PASSWORD')}@{os.getenv('MONGODB_URL')}"
+            # before replica set, standalone 
+            # f"mongodb://{os.getenv('MONGODB_USERNAME')}:{os.getenv('MONGODB_PASSWORD')}@{os.getenv('MONGODB_URL')}"
+            # replica set
+            f"mongodb://{os.getenv('MONGODB_USERNAME')}:{os.getenv('MONGODB_PASSWORD')}@{MONGODB_HOST}:27017/?replicaSet={MONGODB_REPLICA_SET}&authSource={MONGODB_AUTH_SOURCE}"
         )
         db = mongo_client["user_database"]
         users_collection = db.get_collection("users")
